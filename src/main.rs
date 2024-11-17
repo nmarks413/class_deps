b848d38ad3a0fbc48f200c78eb7109504b30427c
Natalie Marks <nmarks413@gmail.com>
2024-11-17 00:45:36 -0800

    parsing working?

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,19 @@ use scraper::{node::Element, ElementRef};
 static URL: &str =
     "https://catalog.ucsc.edu/en/current/general-catalog/courses/am-applied-mathematics/";
 
+#[derive(Debug)]
 enum PE {
     E,
     H,
     T,
 }
+#[derive(Debug)]
 enum PR {
     E,
     C,
     S,
 }
+#[derive(Debug)]
 enum GenEds {
     MF,
     CC,
@@ -26,6 +29,7 @@ enum GenEds {
     C2,
 }
 
+#[derive(Debug)]
 struct Class {
     department: String,
     course_number: (u32, Option<char>),
@@ -33,15 +37,25 @@ struct Class {
     description: String,
     credits: u32,
     requirements: Option<Vec<String>>,
-    gen_ed: Option<Vec<GenEds>>,
+    gen_ed: Option<GenEds>,
     cross_listed: Option<String>,
 }
 
 fn main() {
-    let resp = reqwest::blocking::get(URL);
+    println!(
+        "{:#?}",
+        get_class_list(
+            "https://catalog.ucsc.edu/en/current/general-catalog/courses/math-mathematics/"
+        )
+    );
+    // get_class_list("https://catalog.ucsc.edu/en/current/general-catalog/courses/cse-computer-science-and-engineering");
+}
+
+fn get_class_list(url: &str) -> Vec<Class> {
+    let resp = reqwest::blocking::get(url);
     let html = resp.unwrap().text().unwrap();
 
-    let class_list: Vec<Class> = Vec::new();
+    let mut class_list: Vec<Class> = Vec::new();
 
     let selector = scraper::Selector::parse("div.courselist").unwrap();
     let document = scraper::Html::parse_document(&html);
@@ -57,17 +71,18 @@ fn main() {
         ) {
             // println!("{:?}", curr_class_info);
             if !curr_class_info.is_empty() {
-                Class::new(curr_class_info);
+                class_list.push(Class::new(curr_class_info));
             }
             curr_class_info = Vec::new();
         }
         curr_class_info.push(element);
     }
+
+    class_list
 }
 
 impl Class {
     fn new(input_list: Vec<ElementRef>) -> Self {
-        // println!("{:?}", input_list[0].text().collect::<Vec<_>>());
         let class_dep_num_title = input_list[0].text().collect::<Vec<_>>();
 
         let Some((department, num)) = class_dep_num_title[1].split_once(' ') else {
@@ -99,70 +114,94 @@ impl Class {
 
         let mut index = 5;
 
-        let cross_listed = match input_list[5].value().has_class(
-            "crosslisted",
-            scraper::CaseSensitivity::AsciiCaseInsensitive,
-        ) {
-            true => {
-                index = 7;
-                Some(input_list[6].text().collect::<String>())
-            }
-            false => None,
-        };
+        let mut requirements = None;
 
-        let requirements;
+        let mut gen_ed = None;
 
-        let gen_ed;
+        let mut cross_listed = None;
 
-        if index > input_list.len() {
-            requirements = None;
-            gen_ed = None;
-        } else {
-            requirements = match input_list[index].value().has_class(
-                "extraFields",
+        if index < input_list.len() {
+            cross_listed = match input_list[5].value().has_class(
+                "crosslisted",
                 scraper::CaseSensitivity::AsciiCaseInsensitive,
             ) {
                 true => {
-                    index += 1;
-                    Some(
-                        input_list[index - 1]
-                            .text()
-                            .collect::<String>()
-                            .strip_prefix(':')
-                            .unwrap()
-                            .split(';')
-                            .map(str::to_string)
-                            .collect::<Vec<String>>(),
-                    )
+                    index = 7;
+                    Some(input_list[6].text().collect::<String>())
                 }
                 false => None,
             };
-            if index > input_list.len() {
-                gen_ed = match input_list[index]
-                    .value()
-                    .has_class("gen_ed", scraper::CaseSensitivity::AsciiCaseInsensitive)
-                {
+            if index < input_list.len() {
+                requirements = match input_list[index].value().has_class(
+                    "extraFields",
+                    scraper::CaseSensitivity::AsciiCaseInsensitive,
+                ) {
                     true => {
                         index += 1;
-                        Some(input_list[index - 1].text().collect::<String>())
+                        Some(
+                            input_list[index - 1]
+                                .text()
+                                .collect::<String>()
+                                .split_once(':')
+                                .map(|tuple| {
+                                    tuple
+                                        .1
+                                        .split(';')
+                                        .map(str::trim)
+                                        .map(str::to_string)
+                                        .collect::<Vec<String>>()
+                                }),
+                        )
                     }
                     false => None,
                 };
+                if index < input_list.len() {
+                    gen_ed = match input_list[index]
+                        .value()
+                        .has_class("gen_ed", scraper::CaseSensitivity::AsciiCaseInsensitive)
+                        || input_list[index]
+                            .value()
+                            .has_class("genEd", scraper::CaseSensitivity::AsciiCaseInsensitive)
+                    {
+                        true => GenEds::from_str(input_list[index].text().collect::<Vec<_>>()[2]),
+                        false => None,
+                    };
+                }
             }
         }
 
-        println!("{:?}", input_list[index].text().collect::<Vec<_>>());
         Self {
             department,
             course_number,
             title,
             description,
             credits,
-            requirements,
+            requirements: requirements.flatten(),
             gen_ed,
             cross_listed,
         }
+    }
+}
 
-        // println!("{:?}", input_list[5].text().collect::<Vec<_>>());
+impl GenEds {
+    fn from_str(input: &str) -> Option<GenEds> {
+        match input {
+            "MF" => Some(GenEds::MF),
+            "CC" => Some(GenEds::CC),
+            "ER" => Some(GenEds::ER),
+            "IM" => Some(GenEds::IM),
+            "SI" => Some(GenEds::SI),
+            "SR" => Some(GenEds::SR),
+            "TA" => Some(GenEds::TA),
+            "C1" => Some(GenEds::C1),
+            "C2" => Some(GenEds::C2),
+            "PE-E" => Some(GenEds::PE(PE::E)),
+            "PE-H" => Some(GenEds::PE(PE::H)),
+            "PE-T" => Some(GenEds::PE(PE::T)),
+            "PR-E" => Some(GenEds::PR(PR::E)),
+            "PR-C" => Some(GenEds::PR(PR::C)),
+            "PR-S" => Some(GenEds::PR(PR::S)),
+            _ => None,
+        }
     }
 }
